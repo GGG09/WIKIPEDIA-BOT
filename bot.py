9426b022e255fa8b871591d4db0aab0940c32096
shotni4k <79358404+shotni4k@users.noreply.github.com>
2022-02-27 22:49:35 +0300

    review

--- bot.py
+++ bot.py
@@ -1,26 +1,38 @@
-import telebot, wikipedia, re
+# import telebot, wikipedia, re так лучше не делать импорт ибо падает читабельность 
+# такой вариант импорта уместен если ты импортируешь несколько функций из библиотеки или своего файла 
+import telebot
+import wikipedia
+import re 
+
+
 bot = telebot.TeleBot('TOKEN')
 
-wikipedia.set_lang("ru")
+#wikipedia.set_lang("ru")
+#(
+# эТО БЫЛО БЫ ЛОГИЧНЕЙ ОТПРАВИТЬ В ФУНКЦИЮ КАТОРЯ РАБОТАЕТ С БИБЛЕТЕКОЙ 
+# )
 
-def getwiki(s):
+#def getwiki(s): это нарушение стандарта имменования 
+def get_wiki(s): # в python используеться Snake_Case 
+    wikipedia.set_lang("ru")
     try:
         ny = wikipedia.page(s)
         wikitext=ny.content[:1000]
         wikimas=wikitext.split('.')
         wikimas = wikimas[:-1]
-        wikitext2 = ''
+        wikitext2 = '' # зачем ты её обьявил если и не присвоил значения 
         for x in wikimas:
             if not('==' in x):
                 if(len((x.strip()))>3):
                     wikitext2=wikitext2+x+'.'
-            else:
+            else:  # зачем использывать Else если нет ни какого условия?
                 break
-        wikitext2=re.sub('\([^()]*\)', '',wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '',wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '',wikitext2)
+        # в этом блоке ты  перепресваеваешь три раза значения переменной    
+        wikitext2=re.sub('\([^()]*\)', '',wikitext2) # этот блок переменных не понятен 
+        wikitext2=re.sub('\([^()]*\)', '',wikitext2) # поскольку ты используешь одну и туже переменную и оператор присвоения то 
+        wikitext2=re.sub('\([^()]*\)', '',wikitext2) # в переменной  остаёться данные только  из этой строки 
         return wikitext 
-    except Exception as e:
+    except Exception as e: # тут можно подбрать конкретное исключение а не отлавливать весь класс или убрать запись в перемную
         return 'По данному запросу информации в википедии не найдено'
 
 @bot.message_handler(commands=["start"])
@@ -29,8 +41,7 @@ def start(m, res=False):
 
 @bot.message_handler(content_types=["text"])
 def handler_text(message):
-        bot.send_message(message.chat.id, getwiki(message.text))
+        bot.send_message(message.chat.id, get_wiki(message.text))
 
 bot.polling(none_stop=True, interval=0)
-
 
